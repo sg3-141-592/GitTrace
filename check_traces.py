7b4c3637bc28cbfb5285127a942f172c5cc1d384
Sean Host <sean@host.com>
2019-04-05 21:14:11 +0100

    More work

--- check_traces.py
+++ check_traces.py
@@ -1,3 +1,22 @@
-file = open("test\\req.txt","r")
-for line in file:
-    print(line)
+from colorama import init, Fore, Back, Style
+import os
+
+init()
+
+# Find all of the requirements files
+path = '.\\test\\'
+
+files = []
+for r, d, f in os.walk(path):
+    for file in f:
+        print(r)
+        print(d)
+        print(f)
+        files.append(file)
+
+#print(files)
+
+# file = open("test\\req.txt","r")
+# for line in file:
+#     print(Fore.GREEN + line)
+#     a = 1
