b8f4a4c6df0ffc1ff5ba287f2d6dcf3ce9d4225f
Sean Host <sean@host.com>
2019-04-05 21:24:11 +0100

    Pulling in tags

--- check_traces.py
+++ check_traces.py
@@ -9,12 +9,24 @@ path = '.\\test\\'
 files = []
 for r, d, f in os.walk(path):
     for file in f:
-        print(r)
-        print(d)
-        print(f)
-        files.append(file)
+        if '.txt' in file:
+            files.append(os.path.join(r, file))
 
-#print(files)
+tags = dict()
+
+# Get all the tags out of requirements files
+for file in files:
+    f = open(file, "r")
+    print(Fore.LIGHTBLUE_EX + file)
+    for line in f:
+        # Drop out end of line characters
+        line = line.replace('\n','')
+        # Try and add tags to dictionary
+        if line in tags:
+            print(Fore.RED + "duplicate req: " + line)
+        else:
+            print(Fore.GREEN + "new req: " + line)
+            tags[line] = 1
 
 # file = open("test\\req.txt","r")
 # for line in file:
