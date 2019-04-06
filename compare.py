a3671f76585c37e73fd81a4b87b5d9f5776f747c
unknown <sean@gilligan.me>
2019-04-06 19:35:03 +0100

    Adding new systems tags

--- compare.py
+++ compare.py
@@ -11,15 +11,18 @@ after_trace = get_git_file.analyseTrace(analyse_staging=True)
 # Display new tags
 # Find number added
 def report_trace(key):
-    print(Fore.WHITE + "-- " + key)
+    # Calculate differences
     added = set(after_trace[key]) - set(before_trace[key])
     removed = set(before_trace[key]) - set(after_trace[key])
-    if len(added) > 0:
-        print(Fore.GREEN + "A " + str(added))
-    if len(removed) > 0:
-        print(Fore.RED + "D " + str(removed))
+    # Check if there are any changes
+    if len(added) > 0 or len(removed) > 0:
+        print(Fore.WHITE + "-- " + key)
+        if len(added) > 0:
+            print(Fore.GREEN + "A " + str(added))
+        if len(removed) > 0:
+            print(Fore.RED + "D " + str(removed))
 
-print(Fore.WHITE + "-- Changes --")
+print(Fore.WHITE + "-- Analysing Traceability --")
 report_trace('tags')
 report_trace('noUpTrace')
 report_trace('noDownTrace')
